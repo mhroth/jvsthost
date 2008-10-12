a2ccbba748df564aaf73aad8bee2b83e06e0369d
Martin Roth <mhroth@gmail.com>
2008-10-13 00:48:50 +0100

    Formatting updates.
    Formatting updates to modified files, including removal of unnecessary imports.

--- src/com/synthbot/minihost/AudioThread.java
+++ src/com/synthbot/minihost/AudioThread.java
@@ -1,19 +1,34 @@
+/*
+ *  Copyright 2007, 2008 Martin Roth (mhroth@gmail.com)
+ *                       Matthew Yee-King
+ * 
+ *  This file is part of JVstHost.
+ *
+ *  JVstHost is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JVstHost is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with JVstHost.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.synthbot.minihost;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.midi.ShortMessage;
-
-import java.io.InputStream;
-import java.io.ByteArrayInputStream;
-
-import java.util.Vector;
-
 import com.synthbot.audioplugin.vst.JVstHost;
+import java.util.Vector;
+import javax.sound.midi.ShortMessage;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 
 public class AudioThread implements Runnable {
 
@@ -23,12 +38,10 @@ public class AudioThread implements Runnable {
   private byte[] bOutput;
   private int blockSize;
   private int numOutputs;
-
-  private AudioInputStream audioInputStream;
   private SourceDataLine sourceDataLine;
 
   private Vector<ShortMessage> pendingMidi;
-  
+
   private static final float ShortMaxValueAsFloat = (float) (Short.MAX_VALUE);
 
   public AudioThread(JVstHost vst) {
@@ -36,20 +49,13 @@ public class AudioThread implements Runnable {
     addJVstHost(vst);
     numOutputs = vst.numOutputs();
     blockSize = vst.getBlockSize();
-    
     pendingMidi = new Vector<ShortMessage>();
 
-    sourceDataLine = null;
-    
-    InputStream byteArrayInputStream = new ByteArrayInputStream(bOutput);
-    AudioFormat audioFormat = new AudioFormat((int)vst.getSampleRate(), 16, vst.numOutputs(), true, false);
-    
-    audioInputStream = new AudioInputStream(byteArrayInputStream, audioFormat, 
-					    bOutput.length / audioFormat.getFrameSize());
+    AudioFormat audioFormat = new AudioFormat((int) vst.getSampleRate(), 16, vst.numOutputs(), true, false);
     DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, audioFormat);
-    
-    System.out.println("AudioThread::Sound card data line info:"+dataLineInfo.toString());
-    
+    System.out.println("AudioThread::Sound card data line info:" + dataLineInfo.toString());
+
+    sourceDataLine = null;
     try {
       sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
       sourceDataLine.open(audioFormat, bOutput.length);
@@ -60,15 +66,16 @@ public class AudioThread implements Runnable {
     }
   }
 
-  
-  private byte[] floatsToBytes(float[][] fData, byte[] bData){
-    // convert floats to bytes
+  /**
+   * Converts float audio array to an interleaved audio array of 16-bit samples
+   */
+  private byte[] floatsToBytes(float[][] fData, byte[] bData) {
     int index = 0;
     for (int i = 0; i < blockSize; i++) {
       for (int j = 0; j < numOutputs; j++) {
         short sval = (short) (fData[j][i] * ShortMaxValueAsFloat);
         bData[index++] = (byte) (sval & 0x00FF);
-	bData[index++] = (byte) ((sval & 0xFF00) >> 8);
+        bData[index++] = (byte) ((sval & 0xFF00) >> 8);
       }
     }
     return bData;
@@ -83,13 +90,14 @@ public class AudioThread implements Runnable {
     fOutputs = new float[vst.numOutputs()][vst.getBlockSize()];
     bOutput = new byte[vst.numOutputs() * vst.getBlockSize() * 2];
   }
-  
-  // midi events are collected and passed into the jvsthost every time processReplacing gets called 
-  public synchronized void addMidiMessages(ShortMessage message){
+
+  // midi events are collected and passed into the jvsthost every time
+  // processReplacing gets called
+  public synchronized void addMidiMessages(ShortMessage message) {
     pendingMidi.add(message);
   }
 
-  public synchronized ShortMessage[] getMidiMessages(){
+  public synchronized ShortMessage[] getMidiMessages() {
     ShortMessage[] messages = pendingMidi.toArray(new ShortMessage[0]);
     pendingMidi.clear();
     return messages;
--- src/com/synthbot/minihost/GuiMiniHost.java
+++ src/com/synthbot/minihost/GuiMiniHost.java
@@ -1,3 +1,24 @@
+/*
+ *  Copyright 2007, 2008 Martin Roth (mhroth@gmail.com)
+ *                       Matthew Yee-King
+ * 
+ *  This file is part of JVstHost.
+ *
+ *  JVstHost is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JVstHost is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with JVstHost.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.synthbot.minihost;
 
 import com.synthbot.audioplugin.vst.JVstHost;
@@ -6,9 +27,8 @@ import java.io.File;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.InvalidMidiDataException;
 
-
 public class GuiMiniHost {
-  
+
   private static final float sampleRate = 44100f;
   private static final int blockSize = 4096;
   private JVstHost vst;
@@ -16,9 +36,8 @@ public class GuiMiniHost {
 
   private int channel = 0;
   private int velocity = 127;
-  private int midiNoteNumber = 60; // C4 (middle-C)
 
-  public GuiMiniHost(File vstFile){
+  public GuiMiniHost(File vstFile) {
     vst = null;
     try {
       vst = new JVstHost(vstFile, sampleRate, blockSize);
@@ -26,31 +45,28 @@ public class GuiMiniHost {
       jvle.printStackTrace(System.err);
       System.exit(1);
     }
-    
+
     // start the audio thread
     audioThread = new AudioThread(vst);
     Thread thread = new Thread(audioThread);
     thread.start();
-    
-    // create a midi note on message
-    ShortMessage smNoteOn = new ShortMessage();
-    ShortMessage smNoteOff = new ShortMessage();
 
-    int note;
+    // create a midi note on message
+    ShortMessage midiMessage = new ShortMessage();
     
-    // play a random note every 1000 ms 
+    // play a random note every 1000 ms
     try {
       while (true) {
-	note = (int)(Math.random() * 32) + 64;
-	
-	Thread.sleep(500);
-	smNoteOn.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
-	audioThread.addMidiMessages(smNoteOn);
+        int note = (int) (Math.random() * 24) + 48;
 
-  	Thread.sleep(500);
-  	smNoteOff.setMessage(ShortMessage.NOTE_OFF, channel, note, 0);
-  	audioThread.addMidiMessages(smNoteOff);
-		
+        Thread.sleep(500);
+        midiMessage.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+        audioThread.addMidiMessages(midiMessage);
+
+        Thread.sleep(500);
+        midiMessage.setMessage(ShortMessage.NOTE_OFF, channel, note, 0);
+        audioThread.addMidiMessages(midiMessage);
+
       }
     } catch (InvalidMidiDataException imde) {
       imde.printStackTrace(System.err);
@@ -60,14 +76,14 @@ public class GuiMiniHost {
       System.exit(1);
     }
   }
-  
-  public static void main(String[] args){
+
+  public static void main(String[] args) {
     if (args == null || args.length < 1) {
       System.err.println("Usage: java -jar JVstHost.jar <path to vst plugin>");
       System.exit(0);
     }
-    
+
     GuiMiniHost host = new GuiMiniHost(new File(args[0]));
   }
-  
+
 }
