8e9335b934bfefc52924bff3cec1e5f4fe398b63
Martin Roth <mhroth@gmail.com>
2008-10-19 21:29:08 +0100

    Update to buildJvh_win.sh to include __int64 redefintion, eliminating the need for any source files to be altered.

--- src/com/synthbot/audioplugin/vst/buildJvh_win.sh
+++ src/com/synthbot/audioplugin/vst/buildJvh_win.sh
@@ -1,4 +1,4 @@
-gcc -mno-cygwin -o jvsthost.dll -shared -O3 -w  -Wl,--add-stdcall-alias \
+gcc -mno-cygwin -D__int64="long long" -o jvsthost.dll -shared -O3 -w  -Wl,--add-stdcall-alias \
 -I. \
 -I/cygdrive/c/Java/jdk1.6.0_10/include \
 -I/cygdrive/c/Java/jdk1.6.0_10/include/win32 \
@@ -9,5 +9,5 @@ JVstHost.cpp \
 -lstdc++ 
 
 ls -l jvsthost.dll
-echo copying library to C:/WINDOWS/system32
+echo copying jvsthost.dll to C:/WINDOWS/system32
 cp jvsthost.dll /cygdrive/c/WINDOWS/system32
