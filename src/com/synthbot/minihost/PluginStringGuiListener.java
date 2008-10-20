af8715c9bc1ec0078d0f5988978c67ff2aba3ee2
Matthew Yee-King <matthew@yeeking.net>
2008-10-20 13:38:14 +0100

    Added proper GuiMiniHost and RandomMiniHost

---
+++ src/com/synthbot/minihost/PluginStringGuiListener.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright 2007, 2008 Martin Roth (mhroth@gmail.com)
+ *                       Matthew Yee-King
+ * 
+ *  This file is part of JVstHost.
+ *
+ *  JVstHost is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JVstHost is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with JVstHost.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+package com.synthbot.minihost;
+/**
+ *  <code>PluginStringGuiListener</code> - the host implements this
+ *  and it allows the generic plugin gui to talk to the host in a
+ *  controlled way.
+ *
+ * @author Matthew Yee-King, Martin Roth
+ * @version 1.0
+ */
+public interface PluginStringGuiListener{
+
+  public String setParameter(int index, float value);
+  public float getParameter(int index);
+  public int getNumParameters();
+  public String getParameterDisplay(int index);
+  public String getParameterName(int index);
+  public String getParameterLabel(int index);
+  
+  public void setProgram(int index);
+  public String getProgramName();  
+  
+  public void playNote(int noteNumber, int velocity);
+  
+  
+}
--- src/com/synthbot/minihost/RandomMiniHost.java
+++ src/com/synthbot/minihost/RandomMiniHost.java
@@ -27,87 +27,55 @@ import java.io.File;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.InvalidMidiDataException;
 
-public class GuiMiniHost implements PluginStringGuiListener  {
+public class RandomMiniHost   {
 
   private static final float sampleRate = 44100f;
   private static final int blockSize = 4096;
   private JVstHost vst;
   private AudioThread audioThread;
 
-  private GuiMiniHostListener stringGui;
-  
   private int channel = 0;
   private int velocity = 127;
 
-  public GuiMiniHost(File vstFile) {
+  public RandomMiniHost(File vstFile) {
     vst = null;
-
-    stringGui = new PluginStringGui(this);
     try {
-      System.out.println("GuiMiniHost... about ot load ! "+vstFile.toString());
       vst = new JVstHost(vstFile, sampleRate, blockSize);
     } catch (JVstLoadException jvle) {
       jvle.printStackTrace(System.err);
       System.exit(1);
     }
-    stringGui.generateGui();
 
     // start the audio thread
     audioThread = new AudioThread(vst);
     Thread thread = new Thread(audioThread);
     thread.start();
-  }
 
-
-  // implement the PluginStringGUIListener interface 
-  
-  public String setParameter(int index, float value){
-    vst.setParameter(index, value);
-    return vst.getParameterLabel(index);
-  }
-
-  public float getParameter(int index){
-    return vst.getParameter(index);
-  }
-
-  
-  public void setProgram(int index){
-    vst.setProgram(index);
-  }
-  
-  public String getProgramName(){
-    return vst.getProgramName();
-  }
-  
-  public synchronized void playNote(int note, int velocity){
+    // create a midi note on message
+    ShortMessage midiMessage = new ShortMessage();
+    
+    // play a random note every 1000 ms
     try {
-      ShortMessage midiMessage = new ShortMessage();
-      midiMessage.setMessage(ShortMessage.NOTE_ON, 0, note, velocity);
-      audioThread.addMidiMessages(midiMessage);
+      while (true) {
+        int note = (int) (Math.random() * 24) + 48;
+
+        Thread.sleep(500);
+        midiMessage.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+        audioThread.addMidiMessages(midiMessage);
+
+        Thread.sleep(500);
+        midiMessage.setMessage(ShortMessage.NOTE_OFF, channel, note, 0);
+        audioThread.addMidiMessages(midiMessage);
+
+      }
     } catch (InvalidMidiDataException imde) {
       imde.printStackTrace(System.err);
-      //System.exit(1);
+      System.exit(1);
+    } catch (InterruptedException e) {
+      e.printStackTrace(System.err);
+      System.exit(1);
     }
   }
-
-  public int getNumParameters(){
-    return vst.numParameters();
-  }
-
-  public String getParameterDisplay(int index){
-    return vst.getParameterDisplay(index);
-  }
-  
-  public String getParameterName(int index){
-    return vst.getParameterName(index);
-  }
-  
-  public String getParameterLabel(int index){
-    return vst.getParameterLabel(index);
-  
-  }
-
-
 
   public static void main(String[] args) {
     if (args == null || args.length < 1) {
@@ -115,7 +83,7 @@ public class GuiMiniHost implements PluginStringGuiListener  {
       System.exit(0);
     }
 
-    GuiMiniHost host = new GuiMiniHost(new File(args[0]));
+    RandomMiniHost host = new RandomMiniHost(new File(args[0]));
   }
 
 }
