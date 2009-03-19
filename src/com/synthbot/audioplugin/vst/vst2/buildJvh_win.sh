427ecba8a5346a1231e29efe83bdccad31f20d76
U-MHROTH-F0B8FFE6\mhroth <mhroth@mhroth-f0b8ffe6.(none)>
2009-03-19 18:58:45 +0000

    Update windows build script.

--- src/com/synthbot/audioplugin/vst/vst2/buildJvh_win.sh
+++ src/com/synthbot/audioplugin/vst/vst2/buildJvh_win.sh
@@ -1,7 +1,7 @@
 gcc -mno-cygwin -D__int64="long long" -o jvsthost2.dll -shared -O3 -w -Wl,--add-stdcall-alias \
 -I. \
--I/cygdrive/c/Java/jdk1.6.0_10/include \
--I/cygdrive/c/Java/jdk1.6.0_10/include/win32 \
+-I/cygdrive/c/Java/jdk1.6.0_12/include \
+-I/cygdrive/c/Java/jdk1.6.0_12/include/win32 \
 -I./vst2.x \
 -I/usr/include \
 JVstHost.cpp \
